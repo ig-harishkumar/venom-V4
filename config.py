c56297ac9b0016ec0fcdb820e69f851cc3c104e0
ig-harishkumar <102858042+ig-harishkumar@users.noreply.github.com>
2024-08-09 10:09:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7306834091:AAGqT8XdlU0vb-EfRX4bjPW1pPN88Qd39X0")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("PANDEMICYT")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1355746498")
 
 
 
